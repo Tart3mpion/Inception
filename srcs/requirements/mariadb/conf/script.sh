31a2b1577d8503b229a21f89f0d2373377540278
ldes-cou <ldes-cou@student.fr>
2022-04-11 16:05:14 +0200

    finished, mariadb en root

--- srcs/requirements/mariadb/conf/script.sh
+++ srcs/requirements/mariadb/conf/script.sh
@@ -4,11 +4,14 @@ if [ ! -d /var/lib/mysql/${MYSQL_DB} ]; then
     until mysqladmin ping;do
         sleep 2
     done
-    echo "caca prout"
-    mysql -u root -e "CREATE DATABASE $MYSQL_DB;" 
+    mysql -u root -e "CREATE DATABASE $MYSQL_DB;"
     mysql -u root -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_USER_PASSWORD';" 
     mysql -u root -e "GRANT ALL ON $MYSQL_DB.* TO '$MYSQL_USER'@'%';"
+    mysql -e "DELETE FROM mysql.user WHERE user=''"
+	mysql -e "DELETE FROM mysql.user WHERE user='root'"
     mysql -u root -e "FLUSH PRIVILEGES;"
-    killall mysqld
+    mysqladmin shutdown
+else
+    echo "mariadb already there"
 fi
 mysqld
